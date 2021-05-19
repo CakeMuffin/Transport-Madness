ac05b1c6cfc8bd57a8e9815b2561cd32d27d8743
CakeMuffin <vladimir.druzhinin95@gmail.com>
2021-05-19 06:52:56 +0600

    Refactor: Crates
    Carry some functionality to player class from CarCratesCounter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	public bool InCutscene { get; set; } = true;
+	public Player player;
+
 	private int money = 0;
 
-    public static GameManager Instance { get; set; }
+	public static GameManager Instance { get; set; }
 
 	private void Awake()
 	{
@@ -14,13 +18,38 @@ public class GameManager : MonoBehaviour
 	}
 
 	void Start()
-    {
+	{
 		AddMoney(0);
+	}
+
+	private void Update()
+	{
+		CheckSpeed();
+	}
+
+	private void CheckSpeed()
+	{
+		if (player.Speed < 0.1f && !InCutscene)
+		{
+			Debug.Log("Captain Slow");
+		}
 	}
 
 	public void AddMoney(int value)
 	{
 		money += value;
 		UiManager.Instance.SetMoney(money);
+	}
+
+	// Timeline
+	public void HahdleCutsceneEnter()
+	{
+		InCutscene = true;
+	}
+
+	// Timeline
+	public void HahdleCutsceneExit()
+	{
+		InCutscene = false;
 	}
 }
